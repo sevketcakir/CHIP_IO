b77d84b74537880ebddac705a48cd6f9b0f0b7e7
cakir <cakiaccounts@gmail.com>
2022-04-11 23:24:50 +0300

    Python 3.9 compilation fix When you upgrade to Debian 11, package does not compile with the default python which is version 3.9.x Making declarations in constants.c, as in the original Raspberry PI GPIO, solves the compilation problem.

--- source/constants.c
+++ source/constants.c
@@ -41,6 +41,22 @@ SOFTWARE.
 #include "event_gpio.h"
 #include "common.h"
 
+PyObject *high;
+PyObject *low;
+PyObject *input;
+PyObject *output;
+PyObject *alt0;
+PyObject *pud_off;
+PyObject *pud_up;
+PyObject *pud_down;
+PyObject *rising_edge;
+PyObject *falling_edge;
+PyObject *both_edge;
+PyObject *version;
+PyObject *unknown;
+PyObject *board;
+PyObject *bcm;
+
 void define_constants(PyObject *module)
 {
    high = Py_BuildValue("i", HIGH);
